784a61b0e85e2edb21f2dab81fa97b9d34f821e5
g0hzt <rodrigoqsk@gmail.com>
2025-05-29 10:08:13 -0300

    Fixed: Aceitar números negativos Fixed: Aceitar caracteres inválidos Fixed: Mostrar menu

--- desafio_sistema_bancario_v1.py
+++ desafio_sistema_bancario_v1.py
@@ -19,54 +19,71 @@ def menu():
     )
 
 
-menu()
 while True:
-    opcao_menu = int(input("Opção: "))
-    if opcao_menu == 1:
-        print(
-            "\n========= DEPÓSITO =========\nInforme o valor de depósito\n============================"
-        )
-        # Adiciona o valor digitado para a última posição da lista
-        deposito.append(float(input("R$ ")))
-        # [-1] indica que deve ser utilizada a última posição da lista
-        saldo = saldo + deposito[-1]
-        menu()
 
-    elif opcao_menu == 2:
-        print(
-            "\n========= SAQUE =========\nInforme o valor de saque\n========================="
-        )
-        print(f"Saldo Atual: R$ {saldo}\n=========================")
+    # Exibe o menu toda vez que termina uma operação
+    menu()
 
-        if saque_menu < 3:
-            saque.append(float(input("R$ ")))
-            if saldo < saque[-1]:
-                print(
-                    "Você não possui saldo suficiente para realizar a operação."
-                )
-                # Remove o valor de saque informado para que não apareça no extrato
-                saque.remove(saque[-1])
-            else:
-                if saque[-1] <= 500:
-                    saldo = saldo - saque[-1]
-                    saque_menu += 1
-                else:
-                    print("O limite máximo para saque é de R$ 500.00")
+    try:
+        opcao_menu = int(input("Opção: "))
+
+        if opcao_menu == 1:
+            print(
+                "\n========= DEPÓSITO =========\nInforme o valor de depósito\n============================"
+            )
+            # Adiciona o valor digitado para a última posição da lista
+            deposito.append(float(input("R$ ")))
+
+            if deposito[-1] < 0:
+                print("Valor de depósito não pode ser negativo.")
+                deposito.remove(deposito[-1])
+                continue
+
+            # [-1] indica que deve ser utilizada a última posição da lista
+            saldo = saldo + deposito[-1]
+
+        elif opcao_menu == 2:
+            print(
+                "\n========= SAQUE =========\nInforme o valor de saque\n========================="
+            )
+            print(f"Saldo Atual: R$ {saldo}\n=========================")
+
+            if saque_menu < 3:
+                saque.append(float(input("R$ ")))
+
+                if saque[-1] < 0:
+                    print("Valor de saque não pode ser negativo.")
+                    saque.remove(saque[-1])
+                    continue
+
+                if saldo < saque[-1]:
+                    print(
+                        "Você não possui saldo suficiente para realizar a operação."
+                    )
                     # Remove o valor de saque informado para que não apareça no extrato
                     saque.remove(saque[-1])
-        else:
-            print("Você atingiu o limite máximo de 3 saques por dia.")
-        menu()
+                else:
+                    if saque[-1] <= 500:
+                        saldo = saldo - saque[-1]
+                        saque_menu += 1
+                    else:
+                        print("O limite máximo para saque é de R$ 500.00")
+                        # Remove o valor de saque informado para que não apareça no extrato
+                        saque.remove(saque[-1])
+            else:
+                print("Você atingiu o limite máximo de 3 saques por dia.")
 
-    elif opcao_menu == 3:
-        print("\n======== EXTRATO ========")
-        # Irá varrer a lista baseada no seu tamanho (número de itens)
-        for i in range(len(deposito)):
-            print(f"Depósito {i+1}: R$ {deposito[i]:.2f}")
-        for i in range(len(saque)):
-            print(f"Saque {i+1}: R$ {saque[i]:.2f}")
-        print(f"-------------------------\nSaldo Atual: R$ {saldo:.2f}")
-        menu()
+        elif opcao_menu == 3:
+            print("\n======== EXTRATO ========")
+            # Irá varrer a lista baseada no seu tamanho (número de itens)
+            for i in range(len(deposito)):
+                print(f"Depósito {i+1}: R$ {deposito[i]:.2f}")
+            for i in range(len(saque)):
+                print(f"Saque {i+1}: R$ {saque[i]:.2f}")
+            print(f"-------------------------\nSaldo Atual: R$ {saldo:.2f}")
 
-    elif opcao_menu == 0:
-        break
+        elif opcao_menu == 0:
+            break
+
+    except ValueError:
+        print("Valor digitado é inválido.")
